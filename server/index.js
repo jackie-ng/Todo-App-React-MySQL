ac6da70555e3193985fad585b2b943ab2e9cbf8c
Trang Nguyen <ntttrang2130@gmail.com>
2023-09-29 17:18:02 -0400

    create CREATE/ADD route in server

--- server/index.js
+++ server/index.js
@@ -20,8 +20,18 @@ app.get("/task", (req, res) => {
 })
 
 // Add/Create task
-app.post("/", () => {
+app.post("/task", (req, res) => {
+  const q = "INSERT INTO task(`task_desc`) VALUES (?)";
+  const values = [
+    // req.body.idtask,
+    req.body.task_desc
+  ];
 
+  // console.log(req.body);
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.status(200).json(data);
+  });
 })
 
 //Edit/Update task
