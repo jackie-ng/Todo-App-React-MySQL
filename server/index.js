7a34fcb930aef7d132d3b57adc10bb233a04150e
Trang Nguyen <ntttrang2130@gmail.com>
2023-09-29 16:54:56 -0400

    create GET request

--- server/index.js
+++ server/index.js
@@ -1,15 +1,37 @@
-const express = require('express');
-
+import express from "express";
+import { db } from "./db.js"
 const app = express();
+
 
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "GET ROUTE IS RUNNING VIA APP.GET" })
+// Read All Tasks
+app.get("/task", (req, res) => {
+  const q = "SELECT * FROM task";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    return res.status(200).json(data)
+  })
+})
+
+// Add/Create task
+app.post("/", () => {
+
+})
+
+//Edit/Update task
+app.put("/:id", () => {
+
+})
+
+//Delete task
+app.delete("/:id", () => {
+
 })
 
 // set port, listen for request
